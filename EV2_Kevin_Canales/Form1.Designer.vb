87aaf99a1350bdf8ed9d5d1b78c3cd8d56a0f6dd
KevinCG1423 <kevin.canales.gonzalez@gmail.com>
2024-09-29 03:27:40 -0300

    Agregar archivos de proyecto.

---
+++ EV2_Kevin_Canales/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label5 = New Label()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        ComboBox1 = New ComboBox()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Button2 = New Button()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(165, 48)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(191, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(102, 51)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(38, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "Rut :"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(384, 41)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(153, 40)
+        Button1.TabIndex = 2
+        Button1.Text = "Buscar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(177, 14)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(146, 20)
+        Label2.TabIndex = 3
+        Label2.Text = "Ejemplo 12442223-2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(52, 109)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(71, 20)
+        Label3.TabIndex = 4
+        Label3.Text = "Nombre :"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(44, 158)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(79, 20)
+        Label4.TabIndex = 5
+        Label4.Text = "Apellidos :"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(156, 106)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(246, 27)
+        TextBox2.TabIndex = 6
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(156, 158)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(246, 27)
+        TextBox3.TabIndex = 7
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(65, 209)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(48, 20)
+        Label5.TabIndex = 8
+        Label5.Text = "Sexo :"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(149, 372)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(253, 27)
+        TextBox5.TabIndex = 14
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(146, 440)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(256, 27)
+        TextBox6.TabIndex = 15
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(149, 309)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(253, 28)
+        ComboBox1.TabIndex = 16
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(38, 312)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(64, 20)
+        Label9.TabIndex = 17
+        Label9.Text = "Comuna"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(38, 375)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(63, 20)
+        Label10.TabIndex = 18
+        Label10.Text = "Cuidad :"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(38, 447)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(98, 20)
+        Label11.TabIndex = 19
+        Label11.Text = "Observacion :"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(173, 491)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(183, 43)
+        Button2.TabIndex = 20
+        Button2.Text = "Guardar MYSQL"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(87, 247)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(97, 24)
+        RadioButton1.TabIndex = 21
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Masculino"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(228, 247)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(95, 24)
+        RadioButton2.TabIndex = 22
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Femenino"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(384, 247)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(121, 24)
+        RadioButton3.TabIndex = 23
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "No especifico"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(587, 546)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Button2)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(Label5)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "EVA 2 -MYSQL"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+
+End Class
